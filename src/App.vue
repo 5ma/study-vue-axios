4c91d2a3a1868ce4c9cf38ac617f79bdc2e6e5e0
5ma <bbxxuw@gmail.com>
2023-01-30 00:58:50 +0900

    ✨ axios.create()で新しいインスタンスを作成

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,7 @@
 </template>
 
 <script>
-import axios from "axios";
+import axios from "./axios-auth.js";
 export default {
   data() {
     return {
---
+++ src/axios-auth.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://firestore.googleapis.com/v1/projects/vuejs-axios-88c40/databases/(default)/documents"
+});
+
+export default instance;
