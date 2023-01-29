1ba105392ee516f05369f214baabb96c28e854ce
5ma <bbxxuw@gmail.com>
2023-01-29 19:45:43 +0900

    🐛 エラーが出てしまうためコメントアウト

--- src/main.js
+++ src/main.js
@@ -6,9 +6,9 @@ Vue.config.productionTip = false;
 
 axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-axios-88c40/databases/(default)/documents";
 // headers.commonにすることでPOST/GETメソッド関わらず全てのリクエストに対しての設定
-axios.defaults.headers.common["Authorization"] = "hogehoge";
+// axios.defaults.headers.common["Authorization"] = "hogehoge";
 // headers.getにすることでGETリクエストに対しての設定を書くことが出来る
-axios.defaults.headers.get["Accept"] = "application/json";
+// axios.defaults.headers.get["Accept"] = "application/json";
 
 new Vue({
   render: (h) => h(App),
