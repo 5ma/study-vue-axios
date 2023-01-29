53a484033a50b346733ba5a86c2e463e73e624a9
5ma <bbxxuw@gmail.com>
2023-01-30 00:49:55 +0900

    ✨ axiosのinterceptorsで、リクエストの前処理を共通して行う

--- src/main.js
+++ src/main.js
@@ -10,6 +10,31 @@ axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-axi
 // headers.getにすることでGETリクエストに対しての設定を書くことが出来る
 // axios.defaults.headers.get["Accept"] = "application/json";
 
+const interceptorsRequest = axios.interceptors.request.use(
+  (config) => {
+    console.log("interceptors request", config);
+    return config;
+  },
+  (error) => {
+    // 普通に return errorするとthenに繋がってしまうためrejectする必要がある
+    return Promise.reject(error);
+  }
+);
+
+const interceptorsResponse = axios.interceptors.response.use(
+  (response) => {
+    console.log("interceptors response", response);
+    return response;
+  },
+  (error) => {
+    // 普通に return errorするとthenに繋がってしまうためrejectする必要がある
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.request.eject(interceptorsRequest);
+axios.interceptors.response.eject(interceptorsResponse);
+
 new Vue({
   render: (h) => h(App),
 }).$mount("#app");
