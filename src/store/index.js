67959095b46506dd61ed01f0c1a255bd428e2294
5ma <bbxxuw@gmail.com>
2023-02-05 20:31:21 +0900

    ✨ リフレッシュトークンを更新する

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
-import axios from "../axios-auth.js";
+import axios from "../axios-auth";
 import Vue from "vue";
 import Vuex from "vuex";
 import router from "../router";
+import axiosRefresh from "../axios-refresh";
 
 Vue.use(Vuex);
 
@@ -18,24 +19,42 @@ export default new Vuex.Store({
     },
   },
   actions: {
-    login({ commit }, authData) {
+    login({ commit, dispatch }, authData) {
       axios
-        .post("/accounts:signInWithPassword?key=AIzaSyBC_QWWJkaijJnNB3_fnjRFgDeFZYiZFmQ", {
+        .post(`/accounts:signInWithPassword?key=${process.env.VUE_APP_FIREBASE_WEB_API}`, {
           email: authData.email,
           password: authData.password,
           returnSecureToken: true,
         })
         .then((response) => {
           commit("updateIdToken", response.data.idToken);
+          setTimeout(() => {
+            dispatch("refreshIdToken", response.data.idToken);
+          }, response.data.expiresIn * 1000);
           router.push("/");
         })
         .catch((error) => {
           console.log("error", error);
         });
     },
+    refreshIdToken({ commit, dispatch }, refreshToken) {
+      axiosRefresh
+        .post(`token?key=${process.env.VUE_APP_FIREBASE_WEB_API}`, {
+          grant_type: "refresh_token",
+          refresh_token: refreshToken,
+        })
+        .then((response) => {
+          console.log("updateIdToken", response.data);
+          const newIdToken = response.data.refresh_token;
+          commit("updateIdToken", newIdToken);
+          setTimeout(() => {
+            dispatch("refreshIdToken", newIdToken);
+          }, response.data.expires_in * 1000);
+        });
+    },
     register({ commit }, authData) {
       axios
-        .post("/accounts:signUp?key=AIzaSyBC_QWWJkaijJnNB3_fnjRFgDeFZYiZFmQ", {
+        .post(`/accounts:signUp?key=${process.env.VUE_APP_FIREBASE_WEB_API}`, {
           email: authData.email,
           password: authData.password,
           returnSecureToken: true,
